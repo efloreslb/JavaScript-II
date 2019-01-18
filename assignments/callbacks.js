b3a06b2d26a81906654b1aaddd7506eee57b441e
Edgar F <eflores.ef@gmail.com>
2019-01-17 21:08:30 -0800

    Finished first 4 functions in callbacks.js

--- assignments/callbacks.js
+++ assignments/callbacks.js
@@ -21,29 +21,57 @@ const items = ['Pencil', 'Notebook', 'yo-yo', 'Gum'];
 
 */
 
-function
+/*       Callback Function      */
+function giveResult(parameter) {
+  let result = parameter;
+  return 'The result is ' + result;
+}
+/*        CB Function ends      */
 
 function getLength(arr, cb) {
   // getLength passes the length of the array into the callback.
   return cb(arr.length);
 }
+console.log(getLength(items, giveResult));
 
 function last(arr, cb) {
   // last passes the last item of the array into the callback.
+  return cb(arr[arr.length - 1]);
 }
+console.log(last(items, giveResult));
 
 function sumNums(x, y, cb) {
   // sumNums adds two numbers (x, y) and passes the result to the callback.
+  return cb(x + y);
 }
+console.log(sumNums(3, 7, giveResult));
 
 function multiplyNums(x, y, cb) {
   // multiplyNums multiplies two numbers and passes the result to the callback.
+  return cb(x * y);
+}
+console.log(multiplyNums(33, 3, giveResult));
+
+function giveBoolResult(boolParameter) {
+  return boolParameter;
 }
 
 function contains(item, list, cb) {
   // contains checks if an item is present inside of the given array/list.
   // Pass true to the callback if it is, otherwise pass false.
+  let bool = false;
+  for (let i = 0; i < list.length; i++) {
+    if (list[i] === item) {
+      bool = true;
+      return cb(bool);
+    }
+    else {
+      return cb(bool);
+    }
+  }
 }
+
+contains('Pencil', items, giveBoolResult);
 
 /* STRETCH PROBLEM */
 
